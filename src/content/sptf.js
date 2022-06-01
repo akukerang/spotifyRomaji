d19935ada6184e8eee6cdb4073219290cf7632e6
aku <gabrielsuoth@gmail.com>
2022-06-01 17:03:37 -0400

    bug fixes
    fixed queryselector changing html so path led to null.
    removed to check if isjapanese, if title had any english letters in it at all it didn't run.

--- src/content/sptf.js
+++ src/content/sptf.js
@@ -5,16 +5,12 @@ function getSongPlaying(){
     setTimeout(function(){
         var convert = document.evaluate("//*[@id='main']/div/div[2]/div[2]/footer/div/div[1]/div/div[2]/div[1]/div/div/div/div/span/a", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
         songplaying = convert.textContent;
-    },4000); //makes sure title  is loaded
+    },5000); //makes sure title  is loaded
     setTimeout(function(){
-        if(isJapanese(songplaying)==true){ //if song has japanese characthers, go through
             chrome.runtime.sendMessage(songplaying)
             chrome.runtime.onMessage.addListener(function(romaji, sender, sendResponse){
                 songplayingromaji = romaji; 
             });
-        } else {
-            songplayingromaji = songplaying;
-        }       
     },5000); //gives time for get request
 }
 //timing varies based off connection, need to figure out how to better this
@@ -25,7 +21,7 @@ setInterval(function(){
     getSongPlaying();
     console.log(songplaying);
     console.log(songplayingromaji); 
-    document.querySelector("#main > div > div.Root__top-container > div.Root__now-playing-bar > footer > div > div.vlcT8RVn1uWMsYpq2e5K > div > div.KKJUKKP3FyKKKdRjWDVj > div.tyIiSOWsTxiyox1_LW7F > div > div > div > div > span > a").innerHTML = songplayingromaji;
+    document.querySelector("#main > div > div.Root__top-container > div.Root__now-playing-bar > footer > div > div.OgkbKIVYE_mrNpYESylB > div > div.ZcNcu7WZgOAz_Mkcoff3 > div.Q_174taY6n64ZGC3GsKj > div > div > div > div > span > a").innerHTML = songplayingromaji;
     //sets song title area to songplayingromaji
 },30000) //updates every 30 seconds
 
@@ -34,3 +30,5 @@ setInterval(function(){
 
 
 
+
+document.evaluate("//*[@id='main']/div/div[2]/div[2]/footer/div/div[1]/div/div[2]/div[1]/div/div/div/div", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
